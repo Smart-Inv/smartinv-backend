648e61824fd17951017e43884af90534ec7df256
Javier Martin <jmartinpizarro04@gmail.com>
2025-04-20 14:04:45 +0200

    feat: for prod

--- app/firestore_client.py
+++ app/firestore_client.py
@@ -1,15 +1,11 @@
 import firebase_admin
-from firebase_admin import credentials, firestore
+from firebase_admin import firestore
 from dotenv import load_dotenv
-
 import os
 
 load_dotenv()
 
-cred_path = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
-
 if not firebase_admin._apps:
-    cred = credentials.Certificate(cred_path)
-    firebase_admin.initialize_app(cred)
+    firebase_admin.initialize_app()
 
 db = firestore.client()
