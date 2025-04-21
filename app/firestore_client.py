70ddf06d85a9d65c26d09229584a5c6a30fc2099
Javier Martin <jmartinpizarro04@gmail.com>
2025-04-21 10:49:34 +0200

    enables prod/development

--- app/firestore_client.py
+++ app/firestore_client.py
@@ -1,11 +1,17 @@
-import firebase_admin
-from firebase_admin import firestore
-from dotenv import load_dotenv
 import os
+import firebase_admin
+from firebase_admin import credentials, firestore
+from dotenv import load_dotenv
 
 load_dotenv()
 
 if not firebase_admin._apps:
-    firebase_admin.initialize_app()
+    cred_path = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
+    print(os.path.exists(cred_path))
+    if cred_path and os.path.exists(cred_path):
+        cred = credentials.Certificate(cred_path)
+        firebase_admin.initialize_app(cred)
+    else:
+        firebase_admin.initialize_app()
 
 db = firestore.client()
