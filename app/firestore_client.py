5028eba921e763365a69a698e9d5156d29c23b20
JAVIER MARTIN PIZARRO <jmartinpizarro04@gmail.com>
2025-04-22 09:44:24 +0200

    feat: try for dev-prod development

--- app/firestore_client.py
+++ app/firestore_client.py
@@ -5,16 +5,15 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-#if not firebase_admin._apps:
-#    cred_path = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
-#    print(os.path.exists(cred_path))
-#    if cred_path and os.path.exists(cred_path):
-#        cred = credentials.Certificate(cred_path)
-#        firebase_admin.initialize_app(cred)
-#    else:
-#        firebase_admin.initialize_app()
-        
 if not firebase_admin._apps:
-    firebase_admin.initialize_app()
+    cred_path = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
+    if cred_path and os.path.exists(cred_path):
+        cred = credentials.Certificate(cred_path)
+        firebase_admin.initialize_app(cred)
+    else:
+        firebase_admin.initialize_app()
+        
+#if not firebase_admin._apps:
+#    firebase_admin.initialize_app()
 
 db = firestore.client()
